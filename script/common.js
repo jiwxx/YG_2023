65d627a5a70510c3d88f18b05f3608f2ea10efb5
jiwoo <wldnslzhf@gmail.com>
2023-09-12 18:24:34 +0900

    js css

--- script/common.js
+++ script/common.js
@@ -1,9 +1,13 @@
 const menu = document.querySelector ('#mainMenu')
 const span = document.querySelectorAll ('.menu > #mainMenu > span')
 const nav = document.querySelector ('.menu > nav')
-console.log(menu,nav)
+console.log(menu,nav,span)
 
 nav.style.display = 'none'
 menu.addEventListener('click',function(){
     nav.classList.toggle('active')
+    menu.style.position = 'fixed'
+    span[0].style.backgroundColor = '#fff'
+    span[1].style.backgroundColor = '#fff'
+    span[2].style.backgroundColor = '#fff'
 })
--- script/index.js
+++ script/index.js
@@ -1,6 +1,8 @@
 const pawin_slide = new Swiper('#life_slide',{
     loop:true,
-    autoplay:{delay:3000}, //자동재생
+    autoplay:{
+        disableOnInteraction : false,
+        delay:3000}, //자동재생
     speed:800,
     navigation: { //이전, 다음 내비게이션 연결
         //next,prev 객체 연결 시 부모를 안적으면
@@ -15,7 +17,21 @@ const news_slide = new Swiper("#news_slide",{
     slidesPerView:5,
     centeredSlides:true,
     loop:true,
-    autoplay:{delay:2000},
+    autoplay:{
+        delay:2000,
+        disableOnInteraction : false,
+    },
     speed:800,
     scrollbar:{el:'#news_slide .swiper-scrollbar'},
+    on:{
+        init:function(){
+            this.slides[this.activeIndex].querySelector('.swiper-slide > a').style.transform = 'scale(1)'
+        },
+        slideChange:function(){
+            this.slides.forEach(target => {
+                target.querySelector('.swiper-slide > a').style.transform = 'scale(1)';
+            })
+            this.slides[this.activeIndex].querySelector('.swiper-slide > a').style.transfrom = 'scale(1.2)';
+        }
+    }
 })
